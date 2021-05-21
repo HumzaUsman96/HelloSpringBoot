3ef3b397d65ff8ff5301fc595ddfe760f4179af2
Humza Usman <usmanhu@sheridancollege.ca>
2021-05-21 14:58:29 -0400

    added Hello.html and linked css to file

---
+++ src/main/java/sheridan/usmanhu/hellospringboot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package sheridan.usmanhu.hellospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/Hello")
+    public String hello(){
+        return "Hello";
+    }
+}
