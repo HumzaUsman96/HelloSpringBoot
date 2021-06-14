57af9ac06d8a9326a99bb3d00d5e877570703173
Humza Usman <usmanhu@sheridancollege.ca>
2021-06-13 21:39:52 -0400

    Created NameController.java and took mapping code for Input and Output from HelloController.java and added it to the new NameController.java

---
+++ src/main/java/sheridan/usmanhu/hellospringboot/controller/NameController.java
@@ -0,0 +1,24 @@
+package sheridan.usmanhu.hellospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class NameController {
+
+    @GetMapping("/Input")
+    public String input(){
+
+
+        return "Input";
+    }
+
+    @GetMapping("/Output")
+    public String output(@RequestParam String firstName, String lastName, Model model){
+        model.addAttribute("firstName",firstName);
+        model.addAttribute("lastName",lastName);
+        return "Output";
+    }
+}
