2f35d69d153fb097dbbe003c50b918f2158e8f8f
Humza <usman@sheridancollege.ca>
2021-06-15 05:15:02 -0400

    Moved Index from static folder and added links to it with request parameters and path variables. Also created the IndexController.java

---
+++ src/main/java/sheridan/usmanhu/hellospringboot/controller/IndexController.java
@@ -0,0 +1,13 @@
+package sheridan.usmanhu.hellospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+@GetMapping("/Index")
+    public String index(){
+
+        return "Index";
+    }
+}
